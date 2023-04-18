7d16cac5fec31835de87a01cb6820d1beb69d760
meindertkroese <>
2023-04-18 15:32:41 +0200

    tried to do something with adding app

--- scripts/app.py
+++ scripts/app.py
@@ -9,18 +9,29 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route('/', methods=['GET', 'PUT'])
-def upload_file():
+@app.route('/add/', methods=['GET','POST'])
+def add_app():
+    result = ''
+    app = ''
+    cred = ''
+    url = ''
+    response = make_response(render_template('upload.html'),200)
+    if request.method == 'POST':
+        app = request.form['app']
+        cred = request.form['credentials']
+        url = request.form['redirect']
+        return make_response(render_template('succes.html',result=app),200)
+    return response
+
+@app.route('/find/<app>', methods=['GET'])
+def geet_app(app):
     result = ''
     filename = ''
     response = make_response(render_template('get.html',result=result),200)
     #response.status = '200'
-    if request.method == 'PUT':
+    if request.method == 'GET':
         # reponse code ?
-        response = make_response(render_template('put.html',result=result),201)
-    elif request.method == 'GET':
-        # reponse code ?
-        response = make_response(render_template('get.html',result=result),201)
+        response = make_response(render_template('get.html',result=app),201)
     return response
 
 if __name__ == "__main__":
