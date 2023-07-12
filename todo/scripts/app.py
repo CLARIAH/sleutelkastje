ea6f56a2e99cfaf3446efda4cfc443af7ce83edf
meindertkroese <>
2023-07-12 09:45:05 +0200

    security

--- todo/scripts/app.py
+++ todo/scripts/app.py
@@ -41,7 +41,7 @@ def hello_world():
 
 
 @app.route('/todo', methods=['GET'])
-#@oidc_auth.oidc_auth('default')
+@oidc_auth.oidc_auth('default')
 def get_app(app):
 #    result = cur.execute("SELECT _id FROM application WHERE mnemonic = %s",[app])
 #   read inhoud abcd.todo
