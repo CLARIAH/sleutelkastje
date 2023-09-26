8208c7a6a39b3be20d8f5a5994211879e4832c3c
meindertkroese <>
2023-09-26 13:52:55 +0200

    clean code

--- scripts/app.py
+++ scripts/app.py
@@ -59,8 +59,6 @@ def add_app(app,cred,url):
     cur.execute('INSERT INTO application(mnemonic, credentials, redirect) VALUES (%s, %s, %s)',
             (app,cred,url))
     conn.commit()
-    # if succesful:
-    #    return response
     return make_response(render_template('succes.html',result=app),200)
 
 
@@ -69,10 +67,6 @@ def get_app(app):
     logging.debug(f'get app[{app}]')
     result = cur.execute("SELECT _id FROM application WHERE mnemonic = %s",[app])
     response = make_response(render_template('get.html',result=result),200)
-    #response.status = '200'
-    #if request.method == 'GET':
-        # response code ?
-    #    response = make_response(render_template('get.html',result=app),201)
     return response
 
 # 3. user invited 
@@ -87,16 +81,6 @@ def invite(app,invite):
     conn.commit()
     response = make_response(render_template('invite.html',person=invite,app=app),200)
     return response
-
-#@app.route('/test_login', methods=['GET'])
-#@oidc_auth.oidc_auth('default')
-#def test_inlog():
-#    user_session = UserSession(flask.session)
-#    userinfo = user_session.userinfo
-#    logging.debug(f"eptid: {userinfo['edupersontargetedid']}")
-#    return jsonify(access_token=user_session.access_token,
-#                   id_token=user_session.id_token,
-#                   userinfo=user_session.userinfo)
 
 def get_api_key():
     api_key = 'huc:'
@@ -121,7 +105,7 @@ def register(invite):
     if usr_id!=None:
         return "Invitation has been used"
 
-    #!TODO: maak een nieuwe user aan en sla de user info op als JSON -> usr_id
+    # maak een nieuwe user aan en sla de user info op als JSON -> usr_id
     cur.execute('INSERT INTO users(user_info) VALUES (%s)', (userinfo_str,))
     cur.execute('SELECT LASTVAL()')
     user_id = cur.fetchone()[0]
@@ -173,16 +157,6 @@ def add_func_eptid(app,eptid):
     response = make_response(render_template('new_func.html',app=app,func=eptid),200)
     return response
 
-'''
-POST .../<app>/key
-body = API key (of Satosa token, laten we nu even buiten beschouwing)
-basic authentication = de credentials voor de <app>
-0. credentials horen bij de <app>, zo niet return 401
-1. API key begint met huc:, zo niet return 400
-2. API key is bekend voor deze <app>, zo niet return 401
-3. geef de user info voor de API key terug
-'''
-
     
 # 6b.
 # POST .../<app>/key
@@ -198,20 +172,16 @@ def post_key(appl,key):
     if not key.startswith('huc'):
         return make_response('unknown api key',400)
 #2. API key is bekend voor deze <app>, zo niet return 401
-  # TODO: 
     cur.execute('''SELECT u._id,u.user_info FROM invitation AS i
          JOIN application AS a ON a._id = i.app
          JOIN users AS u ON i.usr = u._id
          JOIN key AS k ON k.usr =u._id
             WHERE k.uuid = %s AND a.mnemonic = %s
 ''',[key,appl])
-#    cur.execute("SELECT usr FROM invitation WHERE app = %s ",[appl])
     res = cur.fetchall()
-    logging(f'result of SELECT: {res}') 
     if not res:
         return make_response('unknown api key',401)
 #3. geef de user info voor de API key terug
-#    cur.execute("SELECT user_info FROM users WHERE _id = %s ",[reusr])
     user_info = res[0]['user_info']
     return make_response(f'user info: {user_info}',200)
 
@@ -222,10 +192,6 @@ def post_appl(appl):
     logging.debug(f'post_appl: {appl}')
     key = request.headers["Authorization"]
     logging.debug(f'key: {key}')
-# basic authentication = de credentials voor de <app>
-#0. credentials horen bij de <app>, zo niet return 401
-# credentials in the route ?
-#        return make_response('no credentials',401)
 #1. API key begint met huc:, zo niet return 400
     if  not key.startswith('huc'):
         return make_response('unknown api key',401)
@@ -268,7 +234,6 @@ try:
                 'password' : os.environ.get('DATABASE_PASSWORD', 'test') }
         
     logging.debug(params)
-    #stderr(params)
     # connect to the PostgreSQL database
     conn = psycopg2.connect(**params)
     # create a new cursor
