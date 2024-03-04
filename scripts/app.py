655c2b16eb8b4fc4a58e5378c70f956f0d83fe4d
meindertkroese <>
2024-03-04 10:16:57 +0100

    get data from application

--- scripts/app.py
+++ scripts/app.py
@@ -230,6 +230,7 @@ def post_key(appl,key):
 def post_appl(appl):
     #TODO 20240304: check that <appl> = the logged in user
     if auth.current_user() != appl and auth.current_role()!= 'sysop':
+        pass
         #TODO 20240304: return unauthorized
     logging.debug(f'post_appl: {appl}')
     logging.debug(f'values: {request.values}')
