bdf7c044f41dd78eb856f94e080bd75185bc7d3f
meindertkroese <>
2024-03-19 16:58:59 +0100

    another try (8)

--- todo/java/src/main/java/nl/knaw/huc/di/todo/Todo.java
+++ todo/java/src/main/java/nl/knaw/huc/di/todo/Todo.java
@@ -1,10 +1,13 @@
 package nl.knaw.huc.di.todo;
 
+import com.sun.net.httpserver.HttpServer;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -14,8 +17,17 @@ public class Todo {
   private final Map<UUID, LoginSessionData> loginSessions;
   private final OpenIdClient openIdClient;
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello world!");
+    int port = 9000;
+    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+    System.out.println("server started at " + port);
+    // server.createContext("/", new RootHandler());
+    // server.createContext("/echoHeader", new EchoHeaderHandler());
+    // server.createContext("/echoGet", new EchoGetHandler());
+    // server.createContext("/echoPost", new EchoPostHandler());
+    server.setExecutor(null);
+    server.start();
   }
 
   public Todo(OpenIdClient openIdClient) {
