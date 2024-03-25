c770adb7514f0d4e8e39af01c587fc179f983e58
meindertkroese <>
2024-03-25 14:16:48 +0100

    read todo file

--- todo/java/src/main/java/nl/knaw/huc/di/todo/Todo.java
+++ todo/java/src/main/java/nl/knaw/huc/di/todo/Todo.java
@@ -3,6 +3,9 @@ package nl.knaw.huc.di.todo;
 import io.javalin.Javalin;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Todo {
 
@@ -10,10 +13,30 @@ public class Todo {
         var app = Javalin.create(/*config*/)
                          .get("/", ctx -> ctx.result("Hello World"))
                          .start(9000);
+        app.get("/todo", ctx -> { // the {} syntax does not allow slashes ('/') as part of the parameter
+            ctx.result(getTodoList());
+        });
+    }
+
+    private String getTodoList() {
+        // get the epid
+        String result = "No todo file. Enjoy your day!";
+        String epid = "";
+        // read the todofiles/{epid}.todo file
+        String fileName = "todofiles/" + epid;
+        try {
+            result = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // return the content
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        Path path = Paths.get("./todofiles");
+        Files.createDirectories(path);
         new Todo();
     }
 }
